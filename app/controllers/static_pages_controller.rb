0b64f5c4395bee975063ed3eb383d8680f33a647
Henry <hnr.leto11@gmail.com>
2020-11-10 17:26:19 -0500

    search by username

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,13 +2,23 @@ class StaticPagesController < ApplicationController
   def index
     flickr = Flickr.new ENV['FLICKR_KEY'], ENV['FLICKR_SECRET']
 
-    if params[:search]
-      if !params[:search][:user].empty?
+    return unless params[:search]
+
+    if !params[:search][:user].empty?
+      begin
         @user = flickr.people.getInfo(user_id: params[:search][:user])
         @photos = flickr.photos.search(user_id: @user.id)
-      else
-        flash[:alert] = 'Failure'
-        redirect_to root_path
+      rescue Flickr::FailedResponse => e
+        flash[:alert] = e.msg.to_s
+        render 'index'
+      end
+    elsif !params[:search][:username].empty?
+      begin
+        @user = flickr.people.findByUsername(username: params[:search][:username])
+        @photos = flickr.photos.search(user_id: @user.id)
+      rescue Flickr::FailedResponse => e
+        flash[:alert] = e.msg.to_s
+        render 'index'
       end
     end
   end
