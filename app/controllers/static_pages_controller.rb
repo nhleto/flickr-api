a3bb471aec3b0c8bf3fc6ec74250c30ad5c36c1d
Henry <hnr.leto11@gmail.com>
2020-11-10 13:05:33 -0500

    controller setup

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,18 @@
 class StaticPagesController < ApplicationController
   def index
-    flickr = FlickRaw.new ENV['FLICKR_KEY'], ENV['FLICKR_SECRET']
-    flickr.list
+    flickr = Flickr.new ENV['FLICKR_KEY'], ENV['FLICKR_SECRET']
+
+    if params[:search]
+      unless params[:search][:user].empty?
+        begin
+          @user = flickr.people.getInfo(user_id: params[:search][:user])
+        rescue Flicker::FailedResponse
+          flash[:alert].now = 'No User Found...'
+        end
+      end
+    end
+
+    
+
   end
 end
